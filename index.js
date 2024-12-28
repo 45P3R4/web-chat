10dfdbbeeeb87468cd997a86edfd4eb11d8cfb3a
ksamoilova <langleybaka@gmail.com>
2024-12-29 02:50:03 +0300

    send messages

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const url = 'https://dummyjson.com/comments/';
 let body = document.getElementById("container");
 let number = 1;
 
+let username = '';
+username = prompt('username: ', username);
+
+
+const messageB = document.getElementById('sendButton');
+const messageI = document.getElementById('messageText');
+messageB.onclick = sendMessage;
+
+function sendMessage() {
+    const newMessage = document.createElement("div");
+        const newAuthor = document.createElement("div");
+        const newText = document.createElement("div");
+        const newTime = document.createElement("div");
+
+        newMessage.setAttribute('class', 'message');
+        newAuthor.setAttribute('class', 'message__author');
+        newAuthor.setAttribute('class', 'me');
+        newText.setAttribute('class', 'message__text');
+        newTime.setAttribute('class', 'message__time');
+
+        let date = new Date();
+
+        newTime.textContent = '[' +
+        (date.getHours() > 9 ? date.getHours() : '0' + date.getHours())  + 
+        ':' + 
+        (date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes()) +
+        ']';
+        
+        newAuthor.textContent = username + ':';
+        newText.textContent = messageI.value;
+        messageI.value = null;
+
+        newMessage.appendChild(newTime);
+        newMessage.appendChild(newAuthor);
+        newMessage.appendChild(newText);
+        body.appendChild(newMessage);
+}
+
 setInterval(() => {
     getMessage(number);
     number++;
