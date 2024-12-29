f57cee673c8f135661c906153da2fdf901da5f81
ksamoilova <langleybaka@gmail.com>
2024-12-29 04:17:58 +0300

    theme saving

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ setInterval(() => {
 
 const themes = [];
 themes.push('lavender', 'sand', 'pink', 'green', 'old-pc');
+if(localStorage.getItem('theme') == null)
+    document.firstElementChild.setAttribute("data-theme", themes[0]);
+else
+    document.firstElementChild.setAttribute("data-theme", themes[localStorage.getItem('theme')]);
+
 let currentTheme = 0;
 const themeB = document.getElementById('themeButton');
 themeB.onclick = changeTheme;
@@ -27,7 +32,7 @@ themeB.onclick = changeTheme;
 function changeTheme() {
     currentTheme = currentTheme < themes.length-1 ? ++currentTheme : 0;
     document.firstElementChild.setAttribute("data-theme", themes[currentTheme]);
-    localStorage.setItem('theme', themes[currentTheme]);
+    localStorage.setItem('theme', currentTheme);
 }
 
 
